bd14fe9e0bfc1c72fcc7a35e9e04c2ff0548ddae
Yuri Young <yuri.young@qq.com>
2020-04-22 23:35:02 +0800

    page layouts configure

---
+++ src/layouts/frontend/Footer.vue
@@ -0,0 +1,32 @@
+<!--  -->
+<template>
+  <v-footer id="footer" dark>
+    <v-container class="text-center">
+      <v-row>
+        <v-col cols="12">
+          <div>Copyright &copy; 2019-{{ (new Date()).getFullYear() }} Yuri, QQ 12319597</div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "FrontendFooter",
+  components: {},
+
+  data: () => ({
+    //
+  }),
+
+  computed: {},
+
+  methods: {},
+
+  mounted() {}
+};
+</script>
+
+<style lang='scss' scoped>
+</style>
---
+++ src/views/errors/404.vue
@@ -0,0 +1,26 @@
+<!--  -->
+<template>
+  <v-container class="text-center" fill-height style="height:calc(100vh - 58px);">
+    <div>404 Not Found</div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ErrorNotFound",
+  components: {},
+
+  data: () => ({
+    //
+  }),
+
+  computed: {},
+
+  methods: {},
+
+  mounted() {}
+};
+</script>
+
+<style lang='scss' scoped>
+</style>
